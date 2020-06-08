29003be2b4c8ef0d47bb9c32c6f72afa9c7e8d2b
陶旭生 <xusheng.txs@gmail.com>
2020-06-08 10:57:52 +0800

    feature：删除 .prettierignore

--- src/pages/simple.vue
+++ src/pages/simple.vue
@@ -1,14 +1,46 @@
 <template>
   <div class="page-simple">
-    <el-button type="mini">按钮</el-button>
+    <el-button type="mini" ref="btn-one">按钮1</el-button>
+    <el-button type="mini" ref="btn-two">按钮2</el-button>
     <iframe src="https://txs1992.github.io/fast-amap/"></iframe>
   </div>
 </template>
 
 <script>
+import {
+  bind as focusBind,
+  unbind as focusUnbind,
+} from '../../packages/index.js'
+
 export default {
   data() {
     return {}
+  },
+
+  mounted() {
+    const btnOne = this.$refs['btn-one']
+    const btnTwo = this.$refs['btn-two']
+
+    if (btnOne && btnTwo) {
+      focusBind(btnOne, this.outsideOne, 'out-one')
+       focusBind(btnTwo, this.outsideTwo, 'out-two')
+    }
+  },
+
+  outsideOne() {
+     console.log('outsideOne')
+     const btnOne = this.$refs['btn-one']
+     if (btnOne) {
+ focusBind(btnOne, this.outsideOne, 'out-one')
+     }
+  },
+
+  outsideTwo() {
+    console.log('outsideTwo')
+    const btnOne = this.$refs['btn-one']
+     if (btnOne) {
+ focusBind(btnOne, this.outsideOne, 'out-one')
+     }
   },
 }
 </script>
